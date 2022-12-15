924863397a5be7cb9ba2548cc8e1590df67c0d03
GunGZN <86140154+GunGZN@users.noreply.github.com>
2022-12-15 07:19:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ install_base() {
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
     echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码${plain}"
-    read -p "确认是否继续?[y/n]": config_confirm
+    read -p "确认是否继续?TESTX[y/n]": config_confirm
     if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
         read -p "请设置您的账户名:" config_account
         echo -e "${yellow}您的账户名将设定为:${config_account}${plain}"
